7f087a4d6d0509e8cfc2789033f2521071fafdfa
Bonniebon <jan.vijfhuizen@outlook.com>
2021-11-10 16:02:42 +0100

    templated camera.

--- VkEngine/Include/Camera2d.h
+++ VkEngine/Include/Camera2d.h
@@ -1,41 +1,16 @@
 #pragma once
-#include "ShaderSet.h"
-#include "VkRenderer/BindingInfo.h"
-#include "DescriptorPool.h"
+#include "Camera.h"
 
-struct alignas(4) Camera2d final
+struct Camera2dUbo final
 {
-	struct Frame final
-	{
-		VkBuffer buffer;
-		VkDeviceMemory memory;
-		VkDescriptorSet descriptor;
-	};
-
-	class System final : public ShaderSet<Camera2d, Frame>
-	{
-	public:
-		typedef Singleton<System> Instance;
-
-		explicit System(uint32_t size);
-		void Cleanup() override;
-		void Update() override;
-
-		[[nodiscard]] VkDescriptorSetLayout GetLayout() const;
-
-	private:
-		void ConstructInstanceFrame(Frame& frame, Camera2d& material, uint32_t denseId) override;
-		void CleanupInstanceFrame(Frame& frame, Camera2d& material, uint32_t denseId) override;
-
-		vi::BindingInfo _bindingInfo{};
-		VkDescriptorSetLayout _descriptorLayout;
-		DescriptorPool _descriptorPool;
-	};
-
-private:
-	struct Ubo final
-	{
-		glm::vec3 position;
-		float aspectRatio;
-	};
+	glm::vec3 position;
+	float aspectRatio;
 };
+
+class Camera2dSystem final : public Camera::System<Camera2dUbo>
+{
+public:
+	explicit Camera2dSystem(uint32_t size);
+	Camera2dUbo CreateUbo(Camera& camera, uint32_t index) override;
+};
+
--- VkEngine/Source/Camera2d.cpp
+++ VkEngine/Source/Camera2d.cpp
@@ -1,84 +1,26 @@
 #include "pch.h"
 #include "Camera2d.h"
-#include "VkRenderer/DescriptorLayoutInfo.h"
 #include "Transform2d.h"
 #include "VkRenderer/WindowSystemGLFW.h"
 
-Camera2d::System::System(const uint32_t size) : ShaderSet<Camera2d, Frame>(size)
+Camera2dSystem::Camera2dSystem(const uint32_t size): System<Camera2dUbo>(size)
 {
-	auto& renderSystem = RenderSystem::Instance::Get();
-	auto& renderer = renderSystem.GetVkRenderer();
-	auto& swapChain = renderSystem.GetSwapChain();
-
-	vi::DescriptorLayoutInfo camLayoutInfo{};
-	_bindingInfo.size = sizeof Ubo;
-	_bindingInfo.flag = VK_SHADER_STAGE_VERTEX_BIT;
-	camLayoutInfo.bindings.push_back(_bindingInfo);
-	_descriptorLayout = renderer.CreateLayout(camLayoutInfo);
-
-	const uint32_t imageCount = swapChain.GetImageCount();
-	VkDescriptorType uboType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-	_descriptorPool.Construct(imageCount * GetSize(), _descriptorLayout, &uboType, 1);
 }
 
-void Camera2d::System::Cleanup()
+Camera2dUbo Camera2dSystem::CreateUbo(Camera&, const uint32_t index)
 {
-	ShaderSet<Camera2d, Frame>::Cleanup();
-
-	_descriptorPool.Cleanup();
-}
-
-void Camera2d::System::Update()
-{
-	ShaderSet<Camera2d, Frame>::Update();
-
 	auto& renderSystem = RenderSystem::Instance::Get();
-	auto& renderer = renderSystem.GetVkRenderer();
 	auto& windowSystem = renderSystem.GetWindowSystem();
-	auto& transforms = Transform2d::System::Instance::Get();
-	auto frames = GetCurrentFrameSet();
 
 	const auto resolution = windowSystem.GetVkInfo().resolution;
 	const float aspectRatio = static_cast<float>(resolution.x) / resolution.y;
 
-	for (const auto [instance, sparseId] : *this)
-	{
-		const uint32_t denseId = GetDenseId(sparseId);
-		auto& frame = frames.Get<Frame>(denseId);
-		auto& transform = transforms[sparseId];
+	auto& transforms = Transform2d::System::Instance::Get();
+	auto& transform = transforms[index];
 
-		Ubo ubo{};
-		ubo.aspectRatio = aspectRatio;
-		ubo.position = glm::vec3(transform.position, 1);
+	Camera2dUbo ubo{};
+	ubo.position = glm::vec3(transform.position, 1);
+	ubo.aspectRatio = aspectRatio;
 
-		renderer.MapMemory(frame.memory, &ubo, 0, 1);
-	}
-}
-
-VkDescriptorSetLayout Camera2d::System::GetLayout() const
-{
-	return _descriptorLayout;
-}
-
-void Camera2d::System::ConstructInstanceFrame(Frame& frame, Camera2d&, const uint32_t)
-{
-	auto& renderSystem = RenderSystem::Instance::Get();
-	auto& renderer = renderSystem.GetVkRenderer();
-
-	frame.descriptor = _descriptorPool.Get();
-
-	frame.buffer = renderer.CreateBuffer<Ubo>(1, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT);
-	frame.memory = renderer.AllocateMemory(frame.buffer, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-	renderer.BindMemory(frame.buffer, frame.memory);
-	renderer.BindBuffer(frame.descriptor, frame.buffer, _bindingInfo, 0, 0);
-}
-
-void Camera2d::System::CleanupInstanceFrame(Frame& frame, Camera2d&, const uint32_t)
-{
-	auto& renderSystem = RenderSystem::Instance::Get();
-	auto& renderer = renderSystem.GetVkRenderer();
-
-	_descriptorPool.Add(frame.descriptor);
-	renderer.FreeMemory(frame.memory);
-	renderer.DestroyBuffer(frame.buffer);
+	return ubo;
 }
